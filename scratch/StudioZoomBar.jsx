e4bc17cb290e12d9732040aff8c5078c1289bf7c
SoLotto Club <110166056+Dictatorsnft@users.noreply.github.com>
2026-07-11 23:21:32 +0500

    commit

--- scratch/StudioZoomBar.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default function StudioZoomBar() {
-  return (
-<div style={{ display: "flex", flexDirection: "column", rowGap: "8px", alignItems: "start", justifyContent: "flex-start", top: "31.9%", right: "16px", bottom: "31.9%", width: "42px", height: "36.1%", position: "absolute" }}></div>
-  );
-}
